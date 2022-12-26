cccfc7d83130e708127d01febe883c8c9120c982
nagyeongkim <nagyeong.kim@partridgesystems.com>
2022-12-26 16:05:29 +0900

    kng:initial commit

---
+++ src/views/Stock.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="stock">
+    <h2>boots is in stock.</h2>
+  </div>
+</template>
+
+// <script src="https://unpkg.com/vue"></script>
+// <script lang="ts">
+// const stock = new Vue({
+//   el: "#stock", //root element
+//   data: {
+//     product: "Boots",
+//   },
+// });
+//
+</script>

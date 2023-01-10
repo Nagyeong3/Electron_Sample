0aa928de13168fba10fc5f8b4a0c73658a35f1df
nagyeongkim <nagyeong.kim@partridgesystems.com>
2023-01-10 13:42:52 +0900

    first commit

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,7 @@
 
 
 /**electron 렌더러 프로세스 생성 -> BrowserWindow객체 사용**/
-import { app, autoUpdater, protocol, BrowserWindow, nativeImage, Menu } from "electron";
+import { app, protocol, BrowserWindow, nativeImage, Menu } from "electron";
 import { createProtocol, installVueDevtools } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
 
@@ -12,7 +12,8 @@ var CircularJSON = require('circular-json');
 const { dialog } = require('electron')
 const isDevelopment = process.env.NODE_ENV !== "production";
 const contextMenu = require('electron-context-menu')
-
+const { autoUpdater } = require("electron-updater")
+const ProgressBar = require('electron-progressbar');
 /**electron 내부 db 사용
 npm install electron-db --save**/
 // const db = require('electron-db');
@@ -356,7 +357,7 @@ app.on("ready", async () => {
 
 
 
-    const template = [
+    const template: Electron.MenuItemConstructorOptions[] = [
       {
         label: 'Exit',
         submenu: [
@@ -381,8 +382,7 @@ app.on("ready", async () => {
           },
           {
             label: "Check for Updates...", role: 'help',
-            //click: autoUpdater.checkForUpdates()
-
+            click: autoUpdater.checkForUpdates()
           },
 
         ]
@@ -425,6 +425,61 @@ app.on("ready", async () => {
 
 
 
+let progressBar: any;
+
+/* 업데이트가 가능한지 확인하는 부분이다.
+업데이트가 가능한 경우 팝업이 뜨면서 업데이트를 하겠냐고 묻는다.
+Update를 클릭하면 업데이트 가능한 파일을 다운로드 받는다. */
+autoUpdater.on('update-available', () => {
+  dialog
+    .showMessageBox({
+      type: 'info',
+      title: 'Update available',
+      message:
+        'A new version of Project is available. Do you want to update now?',
+      buttons: ['Update', 'Later'],
+    })
+    .then((result) => {
+      const buttonIndex = result.response;
+
+      if (buttonIndex === 0) autoUpdater.downloadUpdate();
+    });
+});
+
+/* progress bar가 없으면 업데이트를 다운받는 동안 사용자가 그 내용을 알 수 없기 때문에
+progress bar는 꼭 만들어준다. */
+autoUpdater.once('download-progress', (progressObj: object) => {
+  progressBar = new ProgressBar({
+    text: 'Downloading...',
+    detail: 'Downloading...',
+  });
+
+  progressBar
+    .on('completed', function () {
+      console.info(`completed...`);
+      progressBar.detail = 'Task completed. Exiting...';
+    })
+    .on('aborted', function () {
+      console.info(`aborted...`);
+    });
+});
+
+// 업데이트를 다운받고 나면 업데이트 설치 후 재시작을 요청하는 팝업이 뜬다.
+autoUpdater.on('update-downloaded', () => {
+  progressBar.setCompleted();
+  dialog
+    .showMessageBox({
+      type: 'info',
+      title: 'Update ready',
+      message: 'Install & restart now?',
+      buttons: ['Restart', 'Later'],
+    })
+    .then((result) => {
+      const buttonIndex = result.response;
+
+      if (buttonIndex === 0) autoUpdater.quitAndInstall(false, true);
+    });
+});
 app.on("before-quit", () => {
   //setWebViewLocalStorageToDB();
   console.log("before-quit")
